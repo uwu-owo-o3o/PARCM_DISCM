18bf3b2e47fe65d6fc2b4d4785425ca6d78edc64
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2025-09-08 22:49:28 +0800

    baisc image pixel generation works

--- PARCM_DISCM/main.cpp
+++ PARCM_DISCM/main.cpp
@@ -1,21 +1,26 @@
-#include "SFML/Graphics.hpp"
+#include "color.h"
+#include "vec3.h"
+
+#include <iostream>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            write_color(std::cout, pixel_color);
         }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
     }
-	return 0;
+
+    std::clog << "\rDone.                 \n";
 }
