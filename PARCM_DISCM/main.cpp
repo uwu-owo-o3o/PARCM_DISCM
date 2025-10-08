31396528d7fb8b7698e0f05898ec890c544cc192
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2025-10-08 19:51:38 +0800

    Test Case 3

--- PARCM_DISCM/main.cpp
+++ PARCM_DISCM/main.cpp
@@ -12,43 +12,42 @@ int main() {
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
     world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
-    for (int a = 0; a < 105; a++) {
+    for (int a = 0; a < 5; a++) {
         
         auto choose_mat = random_double();
-        point3 center(random_double(-1, 4), 0.2, random_double(-3, 3));
-        if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-            shared_ptr<material> sphere_material;
+        point3 center(0, (1 + a) * (0.2 * 2.0), 0);
+        shared_ptr<material> sphere_material;
 
-            if (choose_mat < 0.8) {
-                // diffuse
-                auto albedo = color::random() * color::random();
-                sphere_material = make_shared<lambertian>(albedo);
-                world.add(make_shared<sphere>(center, 0.2, sphere_material));
-            }
-            else if (choose_mat < 0.95) {
-                // metal
-                auto albedo = color::random(0.5, 1);
-                auto fuzz = random_double(0, 0.5);
-                sphere_material = make_shared<metal>(albedo, fuzz);
-                world.add(make_shared<sphere>(center, 0.2, sphere_material));
-            }
-            else {
-                // glass
-                sphere_material = make_shared<dielectric>(1.5);
-                world.add(make_shared<sphere>(center, 0.2, sphere_material));
-            }
-         }
+        //if (choose_mat < 0.8) {
+            // diffuse
+            auto albedo = color::random() * color::random();
+            sphere_material = make_shared<lambertian>(albedo);
+            world.add(make_shared<sphere>(center, 0.2, sphere_material));
+        //}
+        //else if (choose_mat < 0.95) {
+        //    // metal
+        //    auto albedo = color::random(0.5, 1);
+        //    auto fuzz = random_double(0, 0.5);
+        //    sphere_material = make_shared<metal>(albedo, fuzz);
+        //    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+        //}
+        //else {
+        //    // glass
+        //    sphere_material = make_shared<dielectric>(1.5);
+        //    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+        //}
+         
         
     }
 
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+    //auto material1 = make_shared<dielectric>(1.5);
+    //world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
 
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+    //auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    //world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
 
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(6, 1, 0), 1.0, material3));
+    //auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    //world.add(make_shared<sphere>(point3(6, 1, 0), 1.0, material3));
 
     //auto material4 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
     //world.add(make_shared<sphere>(point3(0, 1, 2), 1.0, material4));
@@ -66,8 +65,8 @@ int main() {
     cam.lookat = point3(0, 0, 0);
     cam.vup = vec3(0, 1, 0);
 
-    cam.defocus_angle = 0.35;
-    cam.focus_dist = 5;
+    cam.defocus_angle = 0.6;
+    cam.focus_dist = 10;
 
     cam.render(&world);
 
